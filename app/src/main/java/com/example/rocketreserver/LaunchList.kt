858d8c6516d017ffbc44ca354744f0fce7500a97
Marat <almabekov.marat@gmail.com>
2023-09-03 21:24:46 +0600

    Добавление большей информации для списка

--- app/src/main/java/com/example/rocketreserver/LaunchList.kt
+++ app/src/main/java/com/example/rocketreserver/LaunchList.kt
@@ -23,6 +23,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
+import coil.compose.AsyncImage
 
 @Composable
 fun LaunchList(onLaunchClick: (launchId: String) -> Unit) {
@@ -49,14 +50,16 @@ private fun LaunchItem(launch: LaunchListQuery.Launch, onClick: (launchId: Strin
         },
         supportingText = {
             // Site
-            Text(text = "Site...")
+            Text(text = launch.site ?: "")
         },
         leadingContent = {
             // Mission patch
-            Image(
+            AsyncImage(
                 modifier = Modifier.size(68.dp, 68.dp),
-                painter = painterResource(R.drawable.ic_placeholder),
-                contentDescription = "Mission patch"
+                model = launch.mission?.missionPatch,
+                contentDescription = "Mission patch",
+                placeholder = painterResource(id = R.drawable.ic_placeholder),
+                error = painterResource(id = R.drawable.ic_placeholder)
             )
         }
     )
